0e292590d6be5229df88f5a51029cbc28761cbef
igordp <de-paula@campus.technion.ac.il>
2020-06-19 11:09:24 +0300

    push

---
+++ cmake-build-debug/CMakeFiles/ttftps.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ttftps.dir/ftp_template.cpp.o"
+  "CMakeFiles/ttftps.dir/logger.cpp.o"
+  "CMakeFiles/ttftps.dir/main.cpp.o"
+  "CMakeFiles/ttftps.dir/runnable.cpp.o"
+  "CMakeFiles/ttftps.dir/server.cpp.o"
+  "ttftps"
+  "ttftps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ttftps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
